19ea509dff819a004cff1537822b6ba47852ba72
Mateus Bolito <mateusbolito@hotmail.com>
2024-02-06 17:41:14 -0300

    service test

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,23 @@
 package application_test
+
+import (
+	"hexagonal/application"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := gomock.NewController(ctrl.T)
+	persistence := gomock.NewController(ctrl.T)
+	persistence.EXPECT().Call().Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
